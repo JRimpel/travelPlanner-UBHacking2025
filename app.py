4797606d42460027bd9def7574aa2eeb68917130
JRimpel <jrimpel06@gmail.com>
2025-11-08 14:40:30 -0500

    added some templates

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from flask import render_template, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
 
+    
