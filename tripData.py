d823326ef081afe031870b6dc023a7a42853e0fb
odurisile <108595203+odurisile@users.noreply.github.com>
2025-11-09 03:56:57 -0500

    work image generation

--- tripData.py
+++ tripData.py
@@ -25,23 +25,27 @@ class activities:
     def __init__(self, activityList:list[activity]):
         self.activityList = activityList
 class transport:
-    def __init__(self, type:str, fromLocation:str, toLocation:str, cost:int, booked:bool, link):
+    def __init__(self, type:str, fromLocation:str, toLocation:str, cost:int, booked:bool, link, departureTime:str="", arrivalTime:str=""):
         self.type = type
         self.fromLocation = fromLocation
         self.toLocation = toLocation
         self.cost= cost
         self.booked = booked
         self.link = link
+        self.departureTime = departureTime
+        self.arrivalTime = arrivalTime
 class transportations:
     def __init__(self, transportList:list[transport]):
         self.transportList = transportList
 class trip:
-    def __init__(self, tripName:str, dest:dest, hotels:hotels, activities:activities, transportations:transportations, totalCost:int):
+    def __init__(self, tripName:str, dest:dest, hotels:hotels, activities:activities, transportations:transportations, totalCost:int,startDate:str, endDate:str):
         self.tripName = tripName
         self.dest = dest
         self.hotels = hotels
         self.activities = activities
         self.transportations = transportations
+        self.startDate = startDate
+        self.endDate = endDate
         self.totalCost = totalCost
         def calculateTotalCost(self):
             total = 0
@@ -53,3 +57,6 @@ class trip:
                 total += transport.cost
             self.totalCost = total
             return total
+class projects:
+    def __init__(self, tripList:list[trip],ImgURLs:list[str]):
+        self.tripList = tripList
